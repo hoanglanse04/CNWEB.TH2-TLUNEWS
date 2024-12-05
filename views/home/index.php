a162747e5989cc514dfef0b8cae92435cfdc28a8
hoanglanse04 <hoanglando15@gmail.com>
2024-12-05 08:55:23 +0700

    add:html page

---
+++ views/home/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
+  data-sidebar-image="none">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <title>&#x110;&#x103;ng nh&#x1EAD;Tin tức</title>
+  <link rel="shortcut icon" type="image/x-icon" href="../../assets/img/favicon.png" />
+  <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../../assets/plugins/fontawesome/css/fontawesome.min.css" />
+  <link rel="stylesheet" href="../../assets/plugins/fontawesome/css/all.min.css" />
+  <link rel="stylesheet" href="../../assets/css/line-awesome.min.css" />
+  <link rel="stylesheet" href="../../assets/css/material.css" />
+  <link rel="stylesheet" href="../../assets/css/line-awesome.min.css" />
+  <link rel="stylesheet" href="../../assets/css/style.css" />
+  <link rel="stylesheet" href="../../assets/plugins/toastr/toatr.css" />
+
+</head>
+
+<body class="">
+  <div b-ho2uat4v6r="" class="header">
+    <!-- Logo -->
+    <div b-ho2uat4v6r="" class="header-left">
+      <a b-ho2uat4v6r="" href="admin-dashboard.html" class="logo">
+        <img b-ho2uat4v6r="" src="https://s1.vnecdn.net/vnexpress/restruct/i/v9533/v2_2019/pc/graphics/logo.svg" alt="Logo">
+      </a>
+      <a b-ho2uat4v6r="" href="admin-dashboard.html" class="logo collapse-logo">
+        <img b-ho2uat4v6r="" src="https://s1.vnecdn.net/vnexpress/restruct/i/v9533/v2_2019/pc/graphics/logo.svg" alt="Logo">
+      </a>
+      <a b-ho2uat4v6r="" href="admin-dashboard.html" class="logo2">
+        <img b-ho2uat4v6r="" src="https://s1.vnecdn.net/vnexpress/restruct/i/v9533/v2_2019/pc/graphics/logo.svg" width="100" height="100" alt="Logo">
+      </a>
+    </div>
+    <!-- /Logo -->
+
+
+
+    <!-- Header Title -->
+    <div b-ho2uat4v6r="" class="page-title-box">
+      <h3 b-ho2uat4v6r="">Trang tin tức VnExpress</h3>
+    </div>
+    <!-- /Header Title -->
+
+
+    <!-- Header Menu -->
+    <ul b-ho2uat4v6r="" class="nav user-menu">
+
+
+      <li class="nav-item dropdown has-arrow flag-nav">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="true">
+          <span>Vietnam</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" data-popper-placement="bottom-start" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(10.4px, 42.4px, 0px);">
+          <a class="dropdown-item language-change-vn">
+            <img src="/img/flags/vn.png" alt="Flag" height="16"> Vietnam
+          </a>
+          <a class="dropdown-item language-change-en">
+            <img src="/img/flags/us.png" alt="Flag" height="16"> English
+          </a>
+        </div>
+      </li>
+
+
+
+      <li b-ho2uat4v6r="" class="nav-item dropdown has-arrow main-drop">
+        <a b-ho2uat4v6r="" href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+          <span b-ho2uat4v6r="" class="user-img">
+            <span b-ho2uat4v6r="" class="status online"></span>
+          </span>
+          <span b-ho2uat4v6r="" id="user_name">nxbhongduc</span>
+        </a>
+        <div b-ho2uat4v6r="" class="dropdown-menu">
+          <a b-ho2uat4v6r="" class="dropdown-item btn-logout">Đăng xuất</a>
+        </div>
+      </li>
+    </ul>
+    <!-- /Header Menu -->
+    <!-- Mobile Menu -->
+    <div b-ho2uat4v6r="" class="dropdown mobile-user-menu">
+      <a b-ho2uat4v6r="" href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <i b-ho2uat4v6r="" class="fa-solid fa-ellipsis-vertical"></i>
+      </a>
+      <div b-ho2uat4v6r="" class="dropdown-menu dropdown-menu-right">
+        <a b-ho2uat4v6r="" class="dropdown-item btn-logout">Đăng xuất</a>
+      </div>
+    </div>
+    <!-- /Mobile Menu -->
+  </div>
+  <div class="">
+    <div class="row">
+      <div class="trand-right-single d-flex">
+        <div class="trand-right-img">
+          <img src="../../assets/img/" alt="">
+        </div>
+        <div class="trand-right-cap">
+          <span class="color1">Concert</span>
+          <h4><a href="details.html">Welcome To The Best Model Winner Contest</a></h4>
+        </div>
+      </div>
+      <div class="trand-right-single d-flex">
+        <div class="trand-right-img">
+          <img src="assets/img/trending/right2.jpg" alt="">
+        </div>
+        <div class="trand-right-cap">
+          <span class="color3">sea beach</span>
+          <h4><a href="details.html">Welcome To The Best Model Winner Contest</a></h4>
+        </div>
+      </div>
+      <div class="trand-right-single d-flex">
+        <div class="trand-right-img">
+          <img src="assets/img/trending/right3.jpg" alt="">
+        </div>
+        <div class="trand-right-cap">
+          <span class="color2">Bike Show</span>
+          <h4><a href="details.html">Welcome To The Best Model Winner Contest</a></h4>
+        </div>
+      </div>
+      <div class="trand-right-single d-flex">
+        <div class="trand-right-img">
+          <img src="assets/img/trending/right4.jpg" alt="">
+        </div>
+        <div class="trand-right-cap">
+          <span class="color4">See beach</span>
+          <h4><a href="details.html">Welcome To The Best Model Winner Contest</a></h4>
+        </div>
+      </div>
+      <div class="trand-right-single d-flex">
+        <div class="trand-right-img">
+          <img src="assets/img/trending/right5.jpg" alt="">
+        </div>
+        <div class="trand-right-cap">
+          <span class="color1">Skeping</span>
+          <h4><a href="details.html">Welcome To The Best Model Winner Contest</a></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Main Wrapper -->
+
+  <!-- /Main Wrapper -->
+  <script src="../../assets/js/jquery-3.7.1.min.js"></script>
+  <script src="../../assets/js/bootstrap.bundle.min.js"></script>
+  <script src="../../assets/js/const.js"></script>
+  <script src="../../assets/js/uc-helpers.js"></script>
+  <script src="../../assets/js/toast.js"></script>
+  <script src="../../assets/plugins/toastr/toastr.js"></script>
+
+  <script>
+    $("#toggle-password").click(function() {
+      $(this).toggleClass("fa-eye fa-eye-slash");
+      if ($("#password").attr("type") == "password") {
+        $("#password").attr("type", "text");
+      } else {
+        $("#password").attr("type", "password");
+      }
+    });
+  </script>
+
+  <script>
+    $(document).ready(function() {
+
+    })
+  </script>
+
+</body>
+
+</html>
