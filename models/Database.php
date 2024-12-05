f994f0ebe7766abcd06ce783610f182e700e4029
minh12200412~ <minhvuive122004@gmail.com>
2024-12-05 23:49:56 +0700

    Data retrieval function for admin

---
+++ models/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database {
+    private $db;
+    private function connect() {
+        $host = '127.0.0.1';        
+        $db = 'tintuc';  
+        $user = 'root';      
+        $pass = '';         
+        $charset = 'utf8mb4';      
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset"; 
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,       
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+            PDO::ATTR_EMULATE_PREPARES => false,              
+        ];
+
+        try {
+            $this->db = new PDO($dsn, $user, $pass, $options);   
+        } catch (\PDOException $e) {
+            echo "Lỗi kết nối: " . $e->getMessage();
+        }
+    }
+
+    public function getDb() {
+        if ($this->db === null) {
+            $this->connect();  
+        }
+        return $this->db;  
+    }
+}
